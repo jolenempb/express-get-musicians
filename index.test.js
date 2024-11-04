a96db546e6f57446f889750ef4f29f3d8400369d
Jolene <jolene.pereirasantos@gmail.com>
2024-11-04 15:48:15 +0000

    completed task for workshop

--- index.test.js
+++ index.test.js
@@ -3,20 +3,18 @@ const { execSync } = require('child_process');
 execSync('npm install');
 execSync('npm run seed');
 
-const request = require("supertest")
+const request = require("supertest");
 const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
+const { Musician } = require('./models/index');
 const app = require('./src/app');
 const {seedMusician} = require("./seedData");
 
 
 describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
-
-
-
-
-    
-})
+    test("GET / musicians is returning desired information", async () => {
+        const response = await(app).get("/musicians");
+        const responseData = JSON.parse(response.text);
+        expect(response.statusCode).toBe(200);
+        })
+     
+});
--- src/app.js
+++ src/app.js
@@ -6,11 +6,16 @@ const { db } = require("../db/connection")
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
+app.get('/musicians', async (req, res) => {
+    const musician = await Musician.findAll();
+    res.json(musician);
+});
 
-
-
-
-
+app.get('/musicians/:id', async (req, res) => {
+    const id = req.params['id'];
+    const musicianId = await Musician.findByPk(id);
+    res.json(musicianId);
+})
 
 
 module.exports = app;
